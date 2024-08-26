f751484760e1319ff87dadc05021974fda22723a
Ismail hossen <ismailhossen2255@gmail.com>
2024-08-26 20:35:08 +0000

    final

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_LOCAL);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
